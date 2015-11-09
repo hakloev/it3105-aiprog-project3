0bb9ac898d34064e374c3dc7c6789dc34eb927f8
Håkon Ødegård Løvdal <me@hakloev.no>
2015-11-09 12:47:24 +0100

    what am i doing

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from module5 import multiply
 from module5.mnist import mnist_basics
+import matplotlib.pyplot as pyplot
 
 if __name__ == "__main__":
-	mnist_basics.quicktest(99)
+    #  mnist_basics.quicktest()
+    #  mnist_basics.show_avg_digit(3)
 
-	#print(multiply.multiply_func(1, 2))
+    train_set_images, train_set_labels = mnist_basics.load_all_flat_cases(type="training")
+    test_set_images, test_set_labels = mnist_basics.load_all_flat_cases(type="testing")
+
+
+
+    #  pyplot.show(block=True)
+
---
+++ module5/ann.py
@@ -0,0 +1,23 @@
+class HiddenLayer(object):
+
+    def __init__(self):
+
+class ANN(object):
+
+    def __init__(self):
+        pass
+
+    def blind_test(self):
+        """
+        The method blind test must accept a list of sublists, where each sublist is a vector of length 784 corresponding
+        to the raw features of one image: each sublist is a flattened image containing integers in the range [0, 255].
+        These raw features come directly from a flat-case file. The list does NOT contain labels, hence the adjective
+        blind. Your method must produce a flat list of labels predicted by the ann when given each feature vector as
+        input. Items in the labels list must correspond to items in the flattened image list. So if feature sets
+        consists of five image vectors, a 7, two 3’s, an 8 and a 2 (in that order), then if your ann classifies them
+        correctly, it should return this:
+
+        [7,3,3,8,2]
+        :return:
+        """
+        pass
